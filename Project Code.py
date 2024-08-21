44c9c9ecf44f76520aafb1cb4d1d01468b59a881
Sajil Mohamed <57768036+sajilsaju@users.noreply.github.com>
2024-08-21 12:45:32 +0530

    Project Code.py
    Python code of the project

--- Project Code.py
+++ Project Code.py
@@ -1,24 +1,21 @@
 def caesar_cipher(text, shift, mode='E'):
     result = ""
 
-    if mode == 'D':
+ if mode == 'D':
         shift = -shift
-
 
     for char in text:
         if char.isalpha():
             shift_amount = shift % 26
             new_char = ord(char) + shift_amount
 
-
             if char.islower():
                 if new_char > ord('z'):
                     new_char -= 26
                 elif new_char < ord('a'):
                     new_char += 26
-
-
-            elif char.isupper():
+                    
+      elif char.isupper():
                 if new_char > ord('Z'):
                     new_char -= 26
                 elif new_char < ord('A'):
@@ -34,6 +31,7 @@ def caesar_cipher(text, shift, mode='E'):
 message = input("Enter the message here:")
 shift_value = int(input("Enter the value that you want to shift: "))
 mode = input("Enter mode ('E' for encryption and for decryption press 'D'): ")
+
 #output function
 output = caesar_cipher(message, shift_value, mode)
 print(f"Output: {output}")
